189beed90f4656fe71dc7d98b35c265387560968
User1 <user1@example.com>
2025-02-11 16:29:28 -0800

    update a message

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -24,6 +24,9 @@ export default function HomeLayout() {
           title: 'Details',
         }}
       /> */}
+      /*
+      这里不需要写任何东西就可以把目录下的文件都列出来 Screen的目的只是配置bottom tab 的样式
+      */
     </Stack>
   );
 } 
